202030b039936d8df53ba20781e4d9096d73cc3c
Will Walker <willwalkerdev@gmail.com>
2023-09-10 08:06:16 +0000

    redeploy

--- source/index.js
+++ source/index.js
@@ -7,27 +7,8 @@ const cinemeldRoutes = require("./routes/cinemeld");
 
 require("dotenv").config()
 
-const initializeExpressApp = (appOC) => {
+const addAppRoutes = (appOC) => {
     const logger = appOC.get("Logger");
-
-    // allowed webapp origins
-    const corsOptions = {
-        origin: [
-            "https://dev-cinemeld.willwalker.org",
-            "https://cinemeld.willwalker.org",
-            "https://willwalker.org",
-        ],
-        methods: 'OPTIONS',
-    };
-    app.use(cors(corsOptions));
-
-    app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    })
-    
-    app.use(express.json());
     
     // request logger
     app.use((req, res, next) => {
@@ -38,24 +19,42 @@ const initializeExpressApp = (appOC) => {
     // app routes
     app.use("/cinemeld", cinemeldRoutes);
     // app.use("/weather", weatherRoutes);
-    
-    // CORS preflight
-    app.options("*", (req, res) => {
-        res.status(200).send();
-    });
-
-    // healthcheck
-    app.all("/", (req, res) => {
-        res.send("forty-two"); 
-    });
-    
-    // start the app
-    app.listen(process.env.PORT || 8000);
 }
 
-// set up the express app after the appOC is created
 const { appOCPromise } = require("./ioc");
 appOCPromise.then((appOC) => {
-    initializeExpressApp(appOC);
+    addAppRoutes(appOC);
 })
+
+// allowed webapp origins
+const corsOptions = {
+    origin: [
+        "https://dev-cinemeld.willwalker.org",
+        "https://cinemeld.willwalker.org",
+        "https://willwalker.org",
+    ],
+    methods: 'OPTIONS',
+};
+app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+app.use(express.json());
+
+// CORS preflight
+app.options("*", (req, res) => {
+    res.status(200).send();
+});
+
+// healthcheck
+app.all("/", (req, res) => {
+    res.send("forty-two"); 
+});
+
+// start the app
+app.listen(process.env.PORT || 8000);
 
